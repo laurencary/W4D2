bfbe1d6b985052c58599aebd785ec4bf6a0ca180
laurencary <armstronglg720@gmail.com>
2023-04-18 14:42:54 -0700

    starting chess part one

---
+++ chess/pieces/piece.rb
@@ -0,0 +1,27 @@
+class Piece
+    attr_reader :color, :board, :pos
+
+    def initialize(color, board, pos)
+        @color, @board, @pos = color, board, pos
+    end
+
+    def to_s
+    end
+
+    def empty?
+    end
+
+    def valid_moves
+    end
+
+    def pos=(val)
+    end
+
+    def Symbol
+    end
+
+    private
+    def move_into_check?(end_pos)
+    end
+        
+end

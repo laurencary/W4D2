14dd0dcaa87217e35e0c23d7fe0b111c470757aa
laurencary <armstronglg720@gmail.com>
2023-04-18 14:14:47 -0700

    finished class inheritence

--- class_inheritence/employee.rb
+++ class_inheritence/employee.rb
@@ -1,26 +1,9 @@
-require_relative "manager.rb"
-
 class Employee
     attr_reader :name, :title, :salary, :boss
-
     def initialize(name, title, salary, boss)
         @name, @title, @salary, @boss = name, title, salary, boss
     end
-
     def bonus(multiplier)
         @salary * multiplier
     end
-
 end
-
-ned = Manager.new("Ned", "Founder", 1000000,nil)
-darren = Manager.new("darren", "TA Manager", 78000,ned)
-shawna = Employee.new("Shawna", "TA", 12000,darren)
-david = Employee.new("David", "TA", 10000,darren)
-
-ned.direct_reports = [darren]
-darren.direct_reports = [shawna,david]
-
-p ned.bonus(5) # => 500_000
-p darren.bonus(4) # => 88_000
-p david.bonus(3) # => 30_000
--- class_inheritence/manager.rb
+++ class_inheritence/manager.rb
@@ -1,18 +1,27 @@
-require_relative "employee.rb"
-
+require_relative "employee"
 class Manager < Employee
     attr_accessor :direct_reports
     def initialize(name, title, salary, boss)
         super
         @direct_reports = []
     end
-
-    def bonus(multipler)
+    def bonus(multiplier)
         total_bonus = 0
         @direct_reports.each do |employee|
             total_bonus += employee.salary * multiplier
+            if employee.class == Manager
+                total_bonus += employee.bonus(multiplier)
+            end
         end
         total_bonus
     end
-
 end
+ned = Manager.new("Ned", "Founder", 1000000,nil)
+darren = Manager.new("darren", "TA Manager", 78000,ned)
+shawna = Employee.new("Shawna", "TA", 12000,darren)
+david = Employee.new("David", "TA", 10000,darren)
+ned.direct_reports = [darren]
+darren.direct_reports = [shawna,david]
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
